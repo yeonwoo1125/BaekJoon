1dba62059ff7d41294264707cc46d42e7b2fe8f1
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-09 22:03:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 216 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println((int) sc.next().charAt(0));
+        sc.close();
+        
+        
+    }
+}
