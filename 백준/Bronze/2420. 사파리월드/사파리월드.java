42299caa14b14aee01981ddd7635c6dd2cddb7c8
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-11 07:46:25 +0900

    [Bronze IV] Title: 사파리월드, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println(Math.abs(a-b));
+    }
+}
