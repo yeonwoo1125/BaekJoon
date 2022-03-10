016a1481085402789040acab422d4dbb2dc862a0
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-10 15:16:07 +0900

    [Bronze V] Title: R2, Time: 216 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println(2*s - r1);
+    }
+}
