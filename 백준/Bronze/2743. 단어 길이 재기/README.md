011947aa88b3f1030cca9ba3933c6eb6c0133cbe
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-10 13:15:11 +0900

    [Bronze II] Title: 단어 길이 재기, Time: 132 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 단어 길이 재기 - 2743 
+
+[문제 링크](https://www.acmicpc.net/problem/2743) 
+
+### 성능 요약
+
+메모리: 14324 KB, 시간: 132 ms
+
+### 분류
+
+구현(implementation), 문자열(string)
+
---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+        
+    }
+}
