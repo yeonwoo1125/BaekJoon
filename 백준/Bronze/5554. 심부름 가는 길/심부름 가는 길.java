e06eeb245363226750f800fe17d2626467f72ec3
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-13 21:57:25 +0900

    [Bronze V] Title: 심부름 가는 길, Time: 128 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for(int i=0; i<4; i++)
+            sum+=Integer.parseInt(br.readLine());
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+    }
+}
