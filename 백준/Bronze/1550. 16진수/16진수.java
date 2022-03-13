14b75def6db8fdf5832bee89252aca91394d78bb
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-13 13:12:58 +0900

    [Bronze V] Title: 16진수, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.parseInt(sc.next(),16));
+        sc.close();
+        
+    }
+}
