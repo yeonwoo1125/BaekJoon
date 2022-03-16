bc2dae08abef1dbbd20ed8ef2235908f1967487c
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-17 07:41:51 +0900

    [Bronze V] Title: Site Score, Time: 216 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*56+sc.nextInt()*24+sc.nextInt()*14+sc.nextInt()*6);
+    }
+}
