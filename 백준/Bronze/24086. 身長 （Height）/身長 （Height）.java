dda09858961c365057599ec2e4e6020d4ba268f2
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-18 11:27:02 +0900

    [Bronze V] Title: 身長 (Height), Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(Math.abs(sc.nextInt() - sc.nextInt()));
+    }
+}
