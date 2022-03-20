d9dd831d8f5ebaf010c5562708be9aa15f8247ed
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-20 17:48:51 +0900

    [Bronze I] Title: 단어 공부, Time: 472 ms, Memory: 30552 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) -'A']++;
+        }
+
+        int max = 0;
+        char answer = '?';
+        
+        for (int i = 0; i < 26; i++) {
+            if(max < count[i]){
+                max = count[i];
+                answer = (char)(i+'A');
+            } else if (max == count[i]){
+                answer = '?';
+            }
+        }
+        System.out.println(answer);
+    }
+}
