2cb36a89ab19d6baa9a979728ed77abb25df9058
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-24 07:59:43 +0900

    [Bronze II] Title: 알파벳 개수, Time: 152 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] arr = new int[26];
+
+        for(int i=0; i<str.length(); i++){
+            arr[str.charAt(i)-'a']++;
+        }
+        for(int i=0; i<26; i++)
+            System.out.print(arr[i]+" ");
+    }
+}
