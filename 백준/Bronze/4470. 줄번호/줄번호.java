014c5d5aa4537d9c694c936fb1b4610dcc580e13
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-03-24 08:21:49 +0900

    [Bronze II] Title: 줄번호, Time: 152 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<n; i++){
+            System.out.println(i+1+". "+br.readLine());
+        }
+    }
+}
