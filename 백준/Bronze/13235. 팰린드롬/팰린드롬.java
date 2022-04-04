65105301aef55f615f9e2a345aa5766d148b3e60
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-05 08:00:09 +0900

    [Bronze II] Title: 팰린드롬, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.reverse().toString().equals(str) ? "true" : "false");
+    }
+}
