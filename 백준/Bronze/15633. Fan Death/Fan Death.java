a840e1b4617978f2d0c96ca44ee22519568de1f7
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-14 07:54:31 +0900

    [Bronze III] Title: Fan Death, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/15633. Fan Death/Fan Death.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for(int i=1; i<=n; i++)
+            if(n%i == 0) sum+=i;
+        System.out.println(sum*5-24);
+    }
+}
