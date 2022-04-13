fd434a85faf1982c2f64162be41b1df2a44780f2
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-14 07:42:48 +0900

    [Bronze III] Title: 팬들에게 둘러싸인 홍준, Time: 140 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String idol = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+idol+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
