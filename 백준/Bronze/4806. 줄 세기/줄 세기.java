e5e64630c3f3abc495dc209daa34a8884e090ca3
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-24 12:58:44 +0900

    [Bronze II] Title: 줄 세기, Time: 204 ms, Memory: 17448 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = 0;
+        while(sc.hasNext()){
+            sc.nextLine();
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
