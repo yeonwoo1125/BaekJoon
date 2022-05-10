6b5497f4cb70bfb30dc445e302748e0ead9dad86
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-11 00:05:38 +0900

    Delete 프로그래머스/lv1/81301. 숫자 문자열과 영단어 directory

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(String s) {
-    String[] num = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for (int i = 0; i < num.length; i++) {
-            s = s.replaceAll(num[i], String.valueOf(i));
-        }
-        return Integer.parseInt(s);
-    }
-}
