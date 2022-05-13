8ce2e9d933116cee9c89fa7d4893097cbd855250
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-05-13 20:15:45 +0900

    [Silver II] Title: 최대 힙, Time: 1156 ms, Memory: 29524 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(pq.isEmpty()) System.out.println("0");
+                else {
+                    System.out.println(pq.poll());
+                    
+                }
+            }
+            else pq.add(num);
+        }
+    }
+}
